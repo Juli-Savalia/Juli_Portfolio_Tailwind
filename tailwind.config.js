3a1c35ec8011fe0ec89a77d0abef04041af8170b
Juli <3382julisavalia@gmail.com>
2024-09-04 10:07:59 +0530

    Save

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*html'],
+  theme: {
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+      jost: ['Jost','sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
